d7151f5d86b68c879b3b4a4d9454de5413e4eafe
prasanna0911 <mechboyprasanna01@gmail.com>
2024-10-07 18:21:49 +0530

    comment line argumaent added

--- RPS_game.py
+++ RPS_game.py
@@ -1,7 +1,17 @@
 import sys
 import random
+import argparse
 
 arr=['Rock','Paper','Scissor']
+parser = argparse.ArgumentParser(
+        description="Provides a personalized game experience."
+    )
+
+parser.add_argument(
+        "-n", "--name", metavar="name",
+        required=True, help="The name of the person playing the game."
+    )
+args = parser.parse_args()
 
 print('\n--------------\n')
 def rps():
@@ -9,28 +19,28 @@ def rps():
     wincount=0
     losscount=0
     def play_rps():
-        player=input('Enter 1 for Rock \nEnter 2 for Paper \nEnter 3 for Scissor \n')
+        player=input(f'{args.name}, please enter... \n1 for Rock \n2 for Paper \n3 for Scissor \n')
         nonlocal gamecount,wincount,losscount
         if player not in ['1','2','3']:
-            print("You must enter 1, 2, or 3.")
+            print(f"{args.name} please enter 1, 2, or 3.\n")
             play_rps()
         playerchoice=int(player)
         computerchoice = int(random.choice('123'))
         print('')
-        print(f'You Chose {arr[playerchoice-1]}')
+        print(f'{args.name} You Chose {arr[playerchoice-1]}')
         print(f'Computer Chose {arr[computerchoice-1]}')
         print('')
         if playerchoice==computerchoice:
             print('Match Draw')
         elif playerchoice==1 and computerchoice==3:
             wincount+=1
-            print("🎉 You win!")
+            print(f"🎉 {args.name}, You win!")
         elif playerchoice==2 and computerchoice==1:
             wincount+=1
-            print("🎉 You win!")
+            print(f"🎉 {args.name}, You win!")
         elif playerchoice==3 and computerchoice==2:
             wincount+=1
-            print("🎉 You win!")
+            print(f"🎉 {args.name}, You win!")
         else:
             losscount+=1
             print('you lose')
@@ -38,7 +48,7 @@ def rps():
         gamecount+=1
 
         print(f"\nGame count: {gamecount}")
-        print(f"\nPlayer wins: {wincount}")
+        print(f"\n{args.name} wins: {wincount}")
         print(f"\nPython wins: {losscount}")
         while True:
             playagain = input("\nPlay again? \nY for Yes or \nQ to Quit \n\n")
@@ -53,8 +63,7 @@ def rps():
             print(f"\nYou won {wincount} out of {gamecount} games")
             print("\n🎉🎉🎉🎉")
             print("Thank you for playing!\n")
-            print('--------------\n')
-            sys.exit('Bye')
+            sys.exit(f'Bye {args.name}! 👋\n\n--------------\n')
     return play_rps
 
 play=rps()
